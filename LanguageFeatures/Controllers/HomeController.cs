078420cdfe55a70d9ec8139fdf4321b044383b67
vmuzhyk <vmuzhyk@outlook.com>
2021-03-07 18:54:09 +0200

    Use Null Conditional Operator For Products

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
+
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
         public ViewResult Index()
         {
-            return View(new string[] {"C#", "Language", "Features"});
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts()) 
+            { 
+                string name = p?.Name; 
+                decimal? price = p?.Price;
+                string relatedName = p?.Related?.Name;
+                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
+            }
+            return View(results);
         }
     }
 }
