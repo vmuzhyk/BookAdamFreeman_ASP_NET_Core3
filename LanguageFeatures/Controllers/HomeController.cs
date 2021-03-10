a5774a358fe9b4193683d9760c013947c7f60ad5
vmuzhyk <vmuzhyk@outlook.com>
2021-03-11 00:10:46 +0200

    Using Collection Initializer Syntax in the HomeController.cs

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -9,9 +9,9 @@ namespace LanguageFeatures.Controllers
         public ViewResult Index()
         {
             Dictionary<string, Product> products =
-                new Dictionary<string, Product> { 
-                    { "Kayak", new Product { Name = "Kayak", Price = 275M } }, 
-                    { "Lifejacket", new Product { Name = "Lifejacket", Price = 48.95M } } 
+                new Dictionary<string, Product> {
+                    ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
+                    ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M }
                 }; 
             return View("Index", products.Keys);
         }
