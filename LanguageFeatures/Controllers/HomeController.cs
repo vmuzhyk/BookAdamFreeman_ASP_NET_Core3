fd3dd9b490eb099ae68c2098b9c41b505dcfb5cb
vmuzhyk <vmuzhyk@outlook.com>
2021-03-11 00:06:47 +0200

    Initializing a Dictionary in the HomeController.cs

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -8,7 +8,12 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            return View("Index", new string[] { "Bob", "Joe", "Alice" });
+            Dictionary<string, Product> products =
+                new Dictionary<string, Product> { 
+                    { "Kayak", new Product { Name = "Kayak", Price = 275M } }, 
+                    { "Lifejacket", new Product { Name = "Lifejacket", Price = 48.95M } } 
+                }; 
+            return View("Index", products.Keys);
         }
     }
 }
