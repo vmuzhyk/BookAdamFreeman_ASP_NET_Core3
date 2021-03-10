b38b52ea7c948963198d4a25b87ef9561e93ccbb
vmuzhyk <vmuzhyk@outlook.com>
2021-03-11 00:02:32 +0200

    Using object and collection initializer

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -8,15 +8,7 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            List<string> results = new List<string>();
-            foreach (Product p in Product.GetProducts()) 
-            {
-                string name = p?.Name ?? "<No Name>";
-                decimal? price = p?.Price ?? 0;
-                string relatedName = p?.Related?.Name ?? "<None>";
-                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
-            }
-            return View(results);
+            return View("Index", new string[] { "Bob", "Joe", "Alice" });
         }
     }
 }
