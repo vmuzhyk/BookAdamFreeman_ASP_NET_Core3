eb3ab781cc1e797f5a86e9806bbf8d5aceba28e1
vmuzhyk <vmuzhyk@outlook.com>
2021-03-11 21:27:24 +0200

    Pattern Matching

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -8,12 +8,16 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            Dictionary<string, Product> products =
-                new Dictionary<string, Product> {
-                    ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
-                    ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M }
-                }; 
-            return View("Index", products.Keys);
+            object[] data = new object[] { 275M, 29.95M, "apple", "orange", 100, 10 }; 
+            decimal total = 0; 
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] is decimal d)
+                {
+                    total += d;
+                }
+            }
+            return View("Index", new string[] { $"Total: {total:C2}" });
         }
     }
 }
