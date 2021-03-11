f38b044347757b870b948f8102d86c84d2cc7215
vmuzhyk <vmuzhyk@outlook.com>
2021-03-11 23:15:35 +0200

    Using Lambda Expression Methods and Properties

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -2,42 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using LanguageFeatures.Models;
+using System.Linq;
 
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        /*bool FilterByPrice(Product p) 
-        { 
-            return (p?.Price ?? 0) >= 20; 
-        }*/
-        public ViewResult Index()
-        {
-            ShoppingCart cart = new ShoppingCart 
-            { 
-                Products = Product.GetProducts() 
-            };
-            Product[] productArray = 
-            { 
-                new Product {Name = "Kayak", Price = 275M},
-                new Product {Name = "Lifejacket", Price = 48.95M},
-                new Product {Name = "Soccer ball", Price = 19.50M}, 
-                new Product {Name = "Corner flag", Price = 34.95M}
-            };
-            /*Func<Product, bool> nameFilter = delegate (Product prod) 
-            { 
-                return prod?.Name?[0] == 'S'; 
-            }; 
-            decimal priceFilterTotal = productArray.Filter(FilterByPrice).TotalPrices(); 
-            decimal nameFilterTotal = productArray.Filter(nameFilter).TotalPrices();*/
-
-            decimal priceFilterTotal = productArray.Filter(p => (p?.Price ?? 0) >= 20).TotalPrices(); 
-            decimal nameFilterTotal = productArray.Filter(p => p?.Name?[0] == 'S').TotalPrices();
-            return View("Index", new string[] 
-            { 
-                $"Price Total: {priceFilterTotal:C2}", 
-                $"Name Total: {nameFilterTotal:C2}"
-            });
-        }
+        public ViewResult Index() => View(Product.GetProducts().Select(p => p?.Name));
     }
 }
--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -16,6 +16,7 @@ namespace LanguageFeatures.Models
         public decimal? Price { get; set; }
         public Product Related { get; set; }
         public bool InStock { get; } = true;
+        public bool NameBeginsWithS => Name?[0] == 'S';
         public static Product[] GetProducts()
         {
             Product kayak = new Product { Name = "Kayak", Category = "Water Craft", Price = 275M };
