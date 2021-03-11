d0804911c0111264c0570a7572ca22dbc5020d7a
vmuzhyk <vmuzhyk@outlook.com>
2021-03-11 22:26:14 +0200

    Creating Filtering Extension Methods

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -15,15 +15,12 @@ namespace LanguageFeatures.Controllers
             Product[] productArray = 
             { 
                 new Product {Name = "Kayak", Price = 275M},
-                new Product {Name = "Lifejacket", Price = 48.95M} 
-            }; 
-            decimal cartTotal = cart.TotalPrices(); 
-            decimal arrayTotal = productArray.TotalPrices(); 
-            return View("Index", new string[] 
-            { 
-                $"Cart Total: {cartTotal:C2}", 
-                $"Array Total: {arrayTotal:C2}" 
-            });
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M}, 
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+            decimal arrayTotal = productArray.FilterByPrice(20).TotalPrices(); 
+            return View("Index", new string[] { $"Array Total: {arrayTotal:C2}" });
         }
     }
 }
