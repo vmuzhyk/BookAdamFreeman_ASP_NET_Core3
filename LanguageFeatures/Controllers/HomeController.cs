2e7078d77a196433507a75679e893c6b7cf6bc83
vmuzhyk <vmuzhyk@outlook.com>
2021-03-11 23:25:52 +0200

    Using Anonymous Types

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -8,6 +8,20 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() => View(Product.GetProducts().Select(p => p?.Name));
+        public ViewResult Index()
+        {
+            /*var names = new[] { "Kayak", "Lifejacket", "Soccer ball" }; 
+            return View(names);*/
+
+            var products = new[] 
+            { 
+                new { Name = "Kayak", Price = 275M }, 
+                new { Name = "Lifejacket", Price = 48.95M }, 
+                new { Name = "Soccer ball", Price = 19.50M }, 
+                new { Name = "Corner flag", Price = 34.95M } 
+            };
+            //return View(products.Select(p => p.Name));
+            return View(products.Select(p => p.GetType().Name));
+        }
     }
 }
