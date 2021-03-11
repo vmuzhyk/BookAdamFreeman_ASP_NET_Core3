e12eaeb008f77dba24aa4ecc02226d4231ae6815
vmuzhyk <vmuzhyk@outlook.com>
2021-03-11 21:58:11 +0200

    Using Extension Methods

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -8,19 +8,12 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            object[] data = new object[] { 275M, 29.95M, "apple", "orange", 100, 10 }; 
-            decimal total = 0; 
-            for (int i = 0; i < data.Length; i++)
-            {
-                switch (data[i]) { 
-                    case decimal decimalValue: 
-                        total += decimalValue; 
-                        break; 
-                    case int intValue when intValue > 50: 
-                        total += intValue; 
-                        break; }
-            }
-            return View("Index", new string[] { $"Total: {total:C2}" });
+            ShoppingCart cart = new ShoppingCart 
+            { 
+                Products = Product.GetProducts() 
+            }; 
+            decimal cartTotal = cart.TotalPrices();
+            return View("Index", new string[] { $"Total: {cartTotal:C2}" });
         }
     }
 }
