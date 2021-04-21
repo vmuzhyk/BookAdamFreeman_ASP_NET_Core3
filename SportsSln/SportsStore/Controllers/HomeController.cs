a310bfa735ef7cf38e91742abc97ec92263edbe4
vmuzhyk <vmuzhyk@outlook.com>
2021-04-21 22:42:23 +0300

    Displaing page links

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
 using System.Linq;
+using SportsStore.Models.ViewModels;
+
 namespace SportsStore.Controllers 
 {
     public class HomeController : Controller 
@@ -11,8 +13,18 @@ namespace SportsStore.Controllers
         {
             repository = repo;
         }
-        public ViewResult Index(int productPage = 1) => View(repository.Products.OrderBy(p => p.ProductID).
-                                        Skip((productPage - 1) * PageSize).Take(PageSize));
-        
+        public ViewResult Index(int productPage = 1) => View(new ProductsListViewModel 
+        { 
+            Products = repository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((productPage - 1) * PageSize)
+            .Take(PageSize), 
+            PagingInfo = new PagingInfo { 
+                CurrentPage = productPage, 
+                ItemsPerPage = PageSize, 
+                TotalItems = repository.Products.Count() 
+            } 
+        });
+
     }
 }
