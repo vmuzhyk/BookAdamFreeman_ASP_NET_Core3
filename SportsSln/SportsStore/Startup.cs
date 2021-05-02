1d879055a310622f66b5aed756e0766b3ab7531c
vmuzhyk <vmuzhyk@outlook.com>
2021-05-02 20:32:50 +0300

    Adding route to ProductPages, adding Bootstrap stylesheet

--- SportsSln/SportsStore/Startup.cs
+++ SportsSln/SportsStore/Startup.cs
@@ -35,7 +35,9 @@ namespace SportsStore
             app.UseStaticFiles(); 
             app.UseRouting(); 
             app.UseEndpoints(endpoints => 
-            { 
+            {
+                endpoints.MapControllerRoute("pagination", "Products/Page{productPage}",
+                    new { Controller = "Home", action = "Index" });
                 endpoints.MapDefaultControllerRoute(); 
             });
             SeedData.EnsurePopulated(app);
