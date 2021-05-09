d19084b7032837e4a8b9fa59be902f15ec3f7081
vmuzhyk <vmuzhyk@outlook.com>
2021-05-09 14:54:44 +0300

    Implementing the Cart Feature

--- SportsSln/SportsStore/Startup.cs
+++ SportsSln/SportsStore/Startup.cs
@@ -28,12 +28,15 @@ namespace SportsStore
             });
             services.AddScoped<IStoreRepository, EFStoreRepository>();
             services.AddRazorPages();
+            services.AddDistributedMemoryCache();
+            services.AddSession();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
         { 
             app.UseDeveloperExceptionPage(); 
             app.UseStatusCodePages(); 
-            app.UseStaticFiles(); 
+            app.UseStaticFiles();
+            app.UseSession();
             app.UseRouting(); 
             app.UseEndpoints(endpoints => 
             {
